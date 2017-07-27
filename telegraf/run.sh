36b43474dd967e7e1678622d290c929cb4e7987f
Guy D <guyd@satellogic.com>
2017-07-27 17:48:07 +0300

    adding rancher env as the database

--- telegraf/run.sh
+++ telegraf/run.sh
@@ -4,10 +4,12 @@ set -m
 CONFIG_TEMPLATE="/telegraf.template.conf"
 CONFIG_FILE="/etc/telegraf/telegraf.conf"
 
+HOST_NAME=`curl -s http://rancher-metadata/latest/self/host/name`
+RANCHER_ENV=`curl -s  http://rancher-metadata/latest/self/stack/environment_name`
 sed -e "s/\${HOST_NAME}/$HOST_NAME/" \
  -e "s!\${INFLUXDB_HOST}!$INFLUXDB_HOST!" \
  -e "s/\${INFLUXDB_PORT}/$INFLUXDB_PORT/" \
- -e "s/\${DATABASE}/$DATABASE/" \
+ -e "s/\${DATABASE}/$RANCHER_ENV/" \
  $CONFIG_TEMPLATE > $CONFIG_FILE
 
 echo "=> Starting Telegraf ..."
